49c6d896b0927e2fe6df7b88cd29545059f685a2
Sie-vo <info@sievo.de>
2026-05-15 17:18:23 +0200

    PHPMailer Update

--- includes/sendmail_include.php
+++ includes/sendmail_include.php
@@ -95,37 +95,57 @@ function sendemail(string $toname,string $toemail,string $fromname,string $frome
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     //SMTP username
-    $mail->Password   = 'secret';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+	if(!$settings['smtp_host']){
+		$mail->IsMAIL();
+	} else {
+		$mail->SMTPDebug = SMTP::DEBUG_SERVER; // Enable Debug output
+		$mail->IsSMTP();
+		$mail->Host = $settings['smtp_host'];
+		$mail->Port = $settings['smtp_port'];
+		$mail->SMTPAuth = $settings['smtp_auth'] ? true : false;
+		$mail->Username = $settings['smtp_username'];
+		$mail->Password = $settings['smtp_password'];
+		$mail->SMTPAutoTLS = true; // wir probieren es mal so aus. Bei localhost auf false setzen
+		//$mail->SMTPSecure = $settings['smtp_security']; funktioniert so nicht mehr, dann die beiden folgenden
+		//$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+		//$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`;
+	}
+	// set charset
+	$mail->CharSet = $locale['charset'];
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
-
+	$mail->setFrom($fromemail, $fromname);
+	$mail->AddAddress($toemail, $toname);
+	$mail->AddReplyTo($fromemail, $fromname);
+	if ($cc) { 
+		$cc = explode(", ", $cc);
+		foreach ($cc as $ccaddress) {
+			$mail->AddCC($ccaddress);
+		}
+	}
+	if ($bcc) {
+		$bcc = explode(", ", $bcc);
+		foreach ($bcc as $bccaddress) {
+			$mail->AddBCC($bccaddress);
+		}
+	}
+	if ($type == "plain") {
+		$mail->IsHTML(false);
+	} else {
+		$mail->IsHTML(true);
+	}
     //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
     //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Subject = $subject;
+	$mail->Body = $message;
 
     $mail->send();
-    echo 'Message has been sent';
+    return true;
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+	return false;
 }
 }
