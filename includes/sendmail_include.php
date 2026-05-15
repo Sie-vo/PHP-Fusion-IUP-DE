8a32023ed401a038eacf7f3141def38f6a757f15
Sie-vo <info@sievo.de>
2026-05-15 17:23:14 +0200

    PHPMail Update

--- includes/sendmail_include.php
+++ includes/sendmail_include.php
@@ -21,71 +21,19 @@ if (!defined("IN_FUSION")) { die("Access Denied"); }
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-
-function sendemailold($toname, $toemail, $fromname, $fromemail, $subject, $message, $type = "plain", $cc = "", $bcc = "") {
-
-	global $settings, $locale;
-	
-	require_once INCLUDES."class.phpmailer.php";
-	
-	$mail = new PHPMailer();
-	if (file_exists(INCLUDES."language/phpmailer.lang-".$locale['phpmailer'].".php")) {
-		$mail->SetLanguage($locale['phpmailer'], INCLUDES."language/");
-	} else {
-		$mail->SetLanguage("en", INCLUDES."language/");
-	}
-
-	if (!$settings['smtp_host']) {
-		$mail->IsMAIL();
-	} else {
-		$mail->IsSMTP();
-		$mail->Host = $settings['smtp_host'];
-		$mail->Port = $settings['smtp_port'];
-		$mail->SMTPAuth = $settings['smtp_auth'] ? true : false;
-		$mail->Username = $settings['smtp_username'];
-		$mail->Password = $settings['smtp_password'];
-		$mail->SMTPSecure = $settings['smtp_security'];
-	}
-	
-	$mail->CharSet = $locale['charset'];
-	$mail->From = $fromemail;
-	$mail->FromName = $fromname;
-	$mail->AddAddress($toemail, $toname);
-	$mail->AddReplyTo($fromemail, $fromname);
-	if ($cc) { 
-		$cc = explode(", ", $cc);
-		foreach ($cc as $ccaddress) {
-			$mail->AddCC($ccaddress);
-		}
-	}
-	if ($bcc) {
-		$bcc = explode(", ", $bcc);
-		foreach ($bcc as $bccaddress) {
-			$mail->AddBCC($bccaddress);
-		}
-	}
-	if ($type == "plain") {
-		$mail->IsHTML(false);
-	} else {
-		$mail->IsHTML(true);
-	}
-	
-	$mail->Subject = $subject;
-	$mail->Body = $message;
-	
-	if(!$mail->Send()) {
-		$mail->ErrorInfo;
-		$mail->ClearAllRecipients();
-		$mail->ClearReplyTos();
-		return false;
-	} else {
-		$mail->ClearAllRecipients(); 
-		$mail->ClearReplyTos();
-		return true;
-	}
-
-}
-
+/**
+ * Summary of sendemail
+ * @param string $toname
+ * @param string $toemail
+ * @param string $fromname
+ * @param string $fromemail
+ * @param string $subject
+ * @param string $message
+ * @param string $type
+ * @param string $cc
+ * @param string $bcc
+ * @return bool
+ */
 function sendemail(string $toname,string $toemail,string $fromname,string $fromemail,string $subject,string $message, $type = "plain", $cc = "", $bcc = "") {
 	// load Class
 	require 'PHPMailer/src/Exception.php';
@@ -121,7 +69,8 @@ try {
 		$cc = explode(", ", $cc);
 		foreach ($cc as $ccaddress) {
 			$mail->AddCC($ccaddress);
-		}
+}
+
 	}
 	if ($bcc) {
 		$bcc = explode(", ", $bcc);
@@ -145,7 +94,7 @@ try {
     $mail->send();
     return true;
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-	return false;
+		//echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"; Für Fehlersuche auskommentieren
+		return false;
 }
 }
